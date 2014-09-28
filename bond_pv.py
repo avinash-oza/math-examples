5e0cc699051b90717b6e866353a5bf8d59b8031c
Avinash Oza <avioza@gmail.com>
2014-09-28 19:24:28 -0400

    Pre change to n=4

---
+++ bond_pv.py
@@ -0,0 +1,114 @@
+from __future__ import division
+import math
+from simpson_rule import converger
+
+def r_t(t):
+    return 0.0525 + math.log(1+2*t)/200
+
+def r_homework_t(t):
+    return 0.05 + 0.005*math.sqrt(1 + t)
+
+def r_homework2_t(t):
+    return 0.02 + t/(200-t)
+
+def r_example9_t(t):
+    return 0.0525+1/(100*(1+math.exp(-t*t)))
+
+def r_homework9_t(t):
+    return 0.05/(1+2*math.exp(-(1+t)*(1+t)))
+
+def bond_price_inst_rate(cash_flow_times, cash_flow_values, inst_rate_function, tol_values):
+    
+    price = 0
+
+    for i in xrange(len(cash_flow_times)):
+        flow_time = cash_flow_times[i]
+        inst_rate = converger(0, flow_time, inst_rate_function, tol_values[i])
+
+        discount_factor = math.exp(-inst_rate)
+        print "discount_factor for t={0} : {1:.12}".format(flow_time, discount_factor)
+        price += cash_flow_values[i] * discount_factor
+
+    return price
+
+def bond_price(cash_flow_times, cash_flow_values, zero_rate_function):
+    
+    price = 0
+
+    for i in xrange(len(cash_flow_times)):
+        flow_time = cash_flow_times[i]
+        discount_factor = math.exp(-flow_time*zero_rate_function(flow_time))
+        print discount_factor
+        price += cash_flow_values[i] * discount_factor
+
+    return price
+
+def price_duration_convexity(cash_flow_times, cash_flow_values, the_yield):
+    price = 0
+    duration = 0
+    convexity = 0
+
+    for i in xrange(len(cash_flow_times)):
+        flow_time = cash_flow_times[i]
+        discount_factor = math.exp(-flow_time*the_yield)
+        price += cash_flow_values[i]*discount_factor
+        duration += flow_time*cash_flow_values[i]*discount_factor
+        convexity += flow_time*flow_time*cash_flow_values[i]*discount_factor
+
+    return price, duration/price, convexity/price
+
+if __name__ == '__main__':
+#   cash_flow_times = [2/12, 8/12, 14/12, 20/12]
+#   cash_flow_values = [3, 3, 3, 103]
+#   print bond_price(cash_flow_times, cash_flow_values, r_t)
+
+    #Homework values
+#   flow_times = [6/12 ,12/12, 18/12, 24/12]
+#   flow_values = [3.5, 3.5, 3.5, 103.5]
+#   print bond_price(flow_times, flow_values, r_homework_t)
+
+
+#   flow_times = [1/12, 7/12, 13/12, 19/12]
+#   flow_values = [3.5, 3.5, 3.5, 103.5]
+#   print bond_price(flow_times, flow_values, r_homework_t)
+
+    #7a
+ #  flow_times = [7/12, 19/12]
+ #  flow_values = [4, 104]
+ #  print bond_price(flow_times, flow_values, r_homework2_t)
+    #7b
+#   flow_times = [1/12, 7/12, 13/12, 19/12]
+#   flow_values = [2, 2, 2, 102]
+#   print bond_price(flow_times, flow_values, r_homework2_t)
+
+    #7c
+#   flow_times = [1/12, 4/12, 7/12, 10/12, 13/12, 16/12, 19/12]
+#   flow_values = [1, 1, 1, 1, 1, 1, 101]
+#   print "{0:.9f}".format(bond_price(flow_times, flow_values, r_homework2_t))
+    
+    #book_example relevant to 8
+#   flow_times = [2/12, 8/12, 14/12, 20/12]
+#   flow_values = [3, 3, 3, 103]
+#   the_yield = 0.065
+#   p, d, c = price_duration_convexity(flow_times, flow_values, the_yield)
+#   print "{0:.9f} {1:.9f} {2:.9f}".format(p, d, c)
+    #8
+#   flow_times = [1/12, 7/12, 13/12, 19/12]
+#   flow_values = [2, 2, 2, 102]
+#   the_yield = 0.025
+#   p, d, c = price_duration_convexity(flow_times, flow_values, the_yield)
+#   print "{0:.9f} {1:.9f} {2:.9f}".format(p, d, c)
+    
+    #Example for #9
+    flow_times = [2/12, 8/12, 14/12, 20/12]
+    flow_values = [3, 3, 3, 103]
+    tol_values = [math.pow(10,-4), math.pow(10, -4), math.pow(10,-4), math.pow(10,-6)]
+    p = bond_price_inst_rate(flow_times, flow_values, r_example9_t, tol_values)
+    print "{0:.6f}".format(p)
+
+    #9
+#   flow_times = [6/12, 12/12, 18/12, 24/12]
+#   flow_values = [2.5, 2.5, 2.5, 102.5]
+#   tol_values = [math.pow(10,-6), math.pow(10, -6), math.pow(10,-6), math.pow(10,-8)]
+#   p = bond_price_inst_rate(flow_times, flow_values, r_homework9_t, tol_values)
+#   print "{0:.9f}".format(p)
--- simpson_rule.py
+++ simpson_rule.py
@@ -4,21 +4,57 @@ import math
 def f_x(x):
     return math.exp(-x*x)
 
+def N__x(x):
+    return math.exp(-x*x/2)
+
 def simpson_rule(a, b, f, n):
     h = (b-a)/n
     result = (f(a) + f(b))/6
 
     for i in xrange(1, n):
         result += f(a + i*h)/3
-        print i
 
     for i in xrange(1, n+1):
         result += 2*f(a + (i - 0.5)*h)/3
-        print i
     result *= h
     
     return result
 
+def normal_converger(a, b, f, tol):
+    n = 2
+    abs_diff = 999999999999999999999999999
+    val_prev = 1/2 + (1/math.sqrt(2*math.pi))*simpson_rule(a, b, f, n)
+
+    while abs_diff > tol:
+        n *=2
+        val_next = 1/2 + (1/math.sqrt(2*math.pi))*simpson_rule(a, b, f, n)
+        abs_diff = abs(val_prev-val_next)
+
+        print "N={0} simpson_rule = {1:.12f} abs_diff={2}".format(n, val_next, abs_diff)
+        
+        val_prev = val_next
+
+def converger(a, b, f, tol):
+    n = 2
+    abs_diff = 999999999999999999999999999
+    val_prev = simpson_rule(a, b, f, n)
+
+    while abs_diff > tol:
+        n *=2
+        val_next = simpson_rule(a, b, f, n)
+        abs_diff = abs(val_prev-val_next)
+
+        print "N={0} simpson_rule = {1} abs_diff={2}".format(n, val_next, abs_diff)
+        
+        val_prev = val_next
+
+    return val_next
+
 if __name__ == '__main__':
-    print simpson_rule(0, 2, f_x, 4)
-#   print simpson_rule(0, 2, f_x, 256)
+#   print simpson_rule(0, 2, f_x, 4)
+#   print simpson_rule(0, 2, f_x, 100000)
+#   print converger(0, 2, f_x, math.pow(10,-12))
+
+    print normal_converger(0, .1, N__x, math.pow(10,-12))
+    print normal_converger(0, .5, N__x, math.pow(10,-12))
+    print normal_converger(0, 1, N__x, math.pow(10,-12))
