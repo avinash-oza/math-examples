c8f2fa5cc3701af909349382b52404d2b26fa67e
Avinash Oza <avioza@gmail.com>
2014-10-12 22:15:01 -0400

    Fix bug in bisection_method

--- numerical_methods.py
+++ numerical_methods.py
@@ -16,12 +16,12 @@ def bisection_method(a, b, f, tol_approx, tol_int, price_call):
     while max(abs(f(sigma=x_left) - price_call), abs(f(sigma=x_right)- price_call)) > tol_approx or x_right- x_left > tol_int:
         x_solution = (x_left + x_right)/2
         
-        if f(sigma=x_left)*f(sigma=x_solution) < 0:
+        if (f(sigma=x_left) - price_call)*(f(sigma=x_solution) - price_call) < 0:
             x_right = x_solution
         else:
             x_left = x_solution
          
-        print "Guess: {0:0.12f}".format(x_solution)
+#       print "Guess: {0:0.12f}".format(x_solution)
         
     return x_solution
 
@@ -43,7 +43,7 @@ def secant_method(x0, f, tol_approx, price_call):
         x_oldest = x_old
         x_old = x_new
         x_new = x_old - (f(sigma=x_old) - price_call)*(x_old - x_oldest)/(f(sigma=x_old) - f(sigma=x_oldest))
-        print "Guess: {0:0.12f}".format(x_new)
+#       print "Guess: {0:0.12f}".format(x_new)
 
     return x_new
     
@@ -83,16 +83,16 @@ if __name__ == '__main__':
 
     #ef implied_volatility(price_call, S, K, T, q, r, tol):
 #   HW 4 #7
-#   f = partial(black_scholes,t=0, S=40, K=40, T=5/12, q=0.01, r=0.025)
-    
-#   print "IMPLIED VOL: {0:0.12f}".format(
-#   implied_volatility(price_call=2.75, S=40, K=40, T=5/12, q=0.01, r=0.025,initial_guess=0.5))
-    
 #   Test for bisection method
 #   print "IMPLIED VOL VIA bisection method: {0:0.12f}".format(bisection_method(a=-2, b=3, f=test_f, tol_approx=math.pow(10, -6), tol_int=math.pow(10, -9), price_call=0))
 
-#   print "IMPLIED VOL VIA bisection method: {0:0.12f}".format(bisection_method(a=0.0001, b=1, f=f, tol_approx=math.pow(10, -6), tol_int=math.pow(10, -6), price_call=2.75))
-#   print "IMPLIED VOL VIA secant method: {0:0.12f}".format(secant_method(x0=0.5, f=f, tol_approx=math.pow(10, -6),  price_call=2.75))
+    f = partial(black_scholes,t=0, S=40, K=40, T=5/12, q=0.01, r=0.025)
+    
+    print "IMPLIED VOL: {0:0.12f}".format(
+    implied_volatility(price_call=2.75, S=40, K=40, T=5/12, q=0.01, r=0.025,initial_guess=0.5))
+    
+    print "IMPLIED VOL VIA bisection method: {0:0.12f}".format(bisection_method(a=0.0001, b=1, f=f, tol_approx=math.pow(10, -6), tol_int=math.pow(10, -6), price_call=2.75))
+    print "IMPLIED VOL VIA secant method: {0:0.12f}".format(secant_method(x0=0.5, f=f, tol_approx=math.pow(10, -6),  price_call=2.75))
 
 #   HW 4 #10
     put_data = [(1100, 56.2, 130/365),
@@ -115,12 +115,12 @@ if __name__ == '__main__':
                  
                  (1175, 129.1, 306/365),
                  (1200, 115.05, 306/365)]
-    with open('dump.csv', 'wb') as csvfile: 
-        my_writer = csv.writer(csvfile, delimiter=',')
-        for case in call_data:
-            data = ["Call",case[0],case[1],case[2],implied_volatility(price_call=case[1],S=1193.0, K=case[0], T=case[2], q=0.017, r=0.001,initial_guess=0.4)]
-            my_writer.writerow(data)
-            print "Strike: {0}, P_MID: {1}, Num days: {2}, Impl vol:{3:0.12f}".format(case[0], case[1], case[2],
-                    implied_volatility(price_call=case[1],S=1193.0, K=case[0], T=case[2], q=0.017, r=0.001,initial_guess=0.4))
+#   with open('dump.csv', 'wb') as csvfile: 
+#       my_writer = csv.writer(csvfile, delimiter=',')
+#       for case in call_data:
+#           data = ["Call",case[0],case[1],case[2],implied_volatility(price_call=case[1],S=1193.0, K=case[0], T=case[2], q=0.017, r=0.001,initial_guess=0.4)]
+#           my_writer.writerow(data)
+#           print "Strike: {0}, P_MID: {1}, Num days: {2}, Impl vol:{3:0.12f}".format(case[0], case[1], case[2],
+#                   implied_volatility(price_call=case[1],S=1193.0, K=case[0], T=case[2], q=0.017, r=0.001,initial_guess=0.4))
 
     pass
