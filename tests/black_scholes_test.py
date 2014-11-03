4fb2e4e3b5b821003aa08065581550a55c4722a8
Avinash Oza <avioza@gmail.com>
2014-11-02 19:03:19 -0500

    Actually put test

--- tests/black_scholes_test.py
+++ tests/black_scholes_test.py
@@ -16,8 +16,7 @@ class BlackScholesTestCases(unittest.TestCase):
         self.assertAlmostEqual(black_scholes(0,20,40,0.25,0.3,0.03,0.02, option_type='PUT'), 19.9499395465949, places=12)
 
     def test_estimated_black_scholes(self):
-        return
-        self.assertAlmostEqual(estimated_black_scholes(0, 40, 40, 3/12, 0.20, 0.01, 0.05), 1.789613023816, places=12,msg="Need to implement return value")
+        self.assertAlmostEqual(estimated_black_scholes(0, 40, 40, 3/12, 0.20, 0.01, 0.05, option_type='CALL'), 1.789613023816, places=12)
 
     def test_black_scholes_vega(self):
         self.assertAlmostEqual(vega_black_scholes(0, 50, 50, 0.25, 0.3, 0, 0), 9.945545790187, places=12)
