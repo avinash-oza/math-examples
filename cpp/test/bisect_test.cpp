66096a9a2bbd8574454e579554f3a265cfd7ee3c
Avinash Oza <avioza@gmail.com>
2015-01-25 19:44:56 -0500

    Add a main test runner file, make makefile more generic

--- cpp/test/bisect_test.cpp
+++ cpp/test/bisect_test.cpp
@@ -1,12 +1,6 @@
 #include <limits.h>
 #include "gtest/gtest.h"
 
-
 TEST(MyTest, Test1){
     EXPECT_EQ(1, 1);
-}
-
-int main(int argc, char **argv) {
-	testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
 }
