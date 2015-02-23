2e5ac3bbce225df27ab4d8fd1ab7d7ee6d0d44ee
Avinash Oza <avioza@gmail.com>
2015-02-22 21:49:57 -0500

    Add cubic spline method for hw3. Update problem 11 tex file

--- nla/hw1/nla_funcs.R
+++ nla/hw1/nla_funcs.R
@@ -1,3 +1,4 @@
+options(digits=10)
 print_latex<- function(mat,digits=-3,rownames_on=TRUE,colnames_on=TRUE) {
   a <- matrix(rnorm(nrow(mat) *ncol(mat)), nrow(mat) ,ncol(mat))
   l_table <- xtable(mat, digits=digits, align=rep("",ncol(a)+1))
@@ -38,4 +39,41 @@ correlation_calc <- function(corr_vector, covar_matrix) {
   # Formula for correlation is 
   D1_inv <- diag(corr_vector^(-1))
   return (D1_inv%*%covar_matrix%*%D1_inv)
+}
+
+cubic_spline <- function(x, y) {
+  
+  library("Matrix")
+  n <- length(x) -1
+  
+  b <- c(0*1:(4*n))
+  
+  M_bar <- matrix(0,nrow = 4*n,ncol = 4*n)
+  M_bar[1,3] <- 2; M_bar[1,4] <-6*x[1]
+  M_bar[4*n,4*n -1] <- 2;M_bar[4*n,4*n]<-6*x[n+1]
+  
+  for(i in 1:n) {
+    vec_val <- i+1
+    
+    b[4*i-2] = y[vec_val-1]; b[4*i-1] = y[vec_val]
+    
+    M_bar[4*i-2,4*i-3] <- 1;M_bar[4*i-2,4*i-2] <- x[vec_val-1]
+    M_bar[4*i-2,4*i-1] <- (x[vec_val-1])^2; M_bar[4*i-2,4*i] <- (x[vec_val-1])^3
+    M_bar[4*i-1,4*i-3] <- 1;M_bar[4*i-1,4*i-2] <- x[vec_val]
+    M_bar[4*i-1,4*i-1] <- (x[vec_val])^2;M_bar[4*i-1,4*i] <- (x[vec_val])^3
+  }
+  
+  for(i in 1:(n-1)) {
+    vec_val <- i+1
+    
+    M_bar[4*i,4*i-2] <- 1; M_bar[4*i,4*i-1] <- 2*x[vec_val]
+    M_bar[4*i,4*i] <- 3*(x[vec_val])^2; M_bar[4*i,4*i+2] <- -1
+    M_bar[4*i,4*i+3] <- -2*x[vec_val]; M_bar[4*i,4*i+4] <- -3*(x[vec_val])^2
+    M_bar[4*i+1,4*i-1] <- 2; M_bar[4*i +1,4*i] <- 6*x[vec_val]
+    M_bar[4*i+1,4*i+3] <- -2; M_bar[4*i+1,4*i+4] <- -6*x[vec_val]
+  }
+  
+  return (solve(M_bar,b))
+  #return (b)
+  
 }
